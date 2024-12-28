930def0ce51b3b51d6520534591c3e539797fcc3
Daniel <danstraws@gmail.com>
2024-12-28 13:03:19 -0600

    fixed pathing issues, package.json script errors, general setup. reset to the init commit

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,12 @@
-import express from 'express';
-import path from 'node:path';
-import db from './config/connection.js';
-import routes from './routes/index.js';
+import express from "express";
+import path from "node:path";
+import db from "./config/connection.js";
+import routes from "./routes/index.js";
+import { Request, Response } from "express";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,13 +14,14 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+app.use(express.static(path.join(__dirname, "../../client/dist")));
+
+app.get("*", (_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+});
 
 app.use(routes);
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
